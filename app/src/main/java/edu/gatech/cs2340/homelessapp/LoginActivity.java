6efd2c601f2a2f8a8c9e3c98a5eaf4331ba7c343
amitej <v.amitej@gmail.com>
2018-02-11 17:42:45 -0500

    Added logout button and linked the activities

--- app/src/main/java/edu/gatech/cs2340/homelessapp/LoginActivity.java
+++ app/src/main/java/edu/gatech/cs2340/homelessapp/LoginActivity.java
@@ -3,6 +3,7 @@ package edu.gatech.cs2340.homelessapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -125,20 +126,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     /**
-     * Callback received when a permissions request has been completed.
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populateAutoComplete();
-            }
-        }
-    }
-
-
-    /**
      * Attempts to sign in or register the account specified by the login form.
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
@@ -186,7 +173,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // perform the user login attempt.
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+            Intent newIntent = new Intent(LoginActivity.this, MainScreenActivity.class);
+            startActivity(newIntent);
         }
     }
 
