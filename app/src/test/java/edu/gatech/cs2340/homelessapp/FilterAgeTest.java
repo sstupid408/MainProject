242573785b0982f0288ba6a65e29c17e5a95aa9b
amitej <v.amitej@gmail.com>
2018-04-06 00:12:25 -0400

    made junit test for filerAge method in FilterActivity

--- app/src/test/java/edu/gatech/cs2340/homelessapp/FilterAgeTest.java
+++ app/src/test/java/edu/gatech/cs2340/homelessapp/FilterAgeTest.java
@@ -2,6 +2,7 @@ package edu.gatech.cs2340.homelessapp;
 
 import android.util.Log;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,8 +13,6 @@ import edu.gatech.cs2340.homelessapp.Controllers.FilterActivity;
 import edu.gatech.cs2340.homelessapp.Model.HomelessShelter;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Amitej Venapally on 4/5/18.
@@ -21,44 +20,84 @@ import static org.junit.Assert.assertTrue;
 
 public class FilterAgeTest {
 
-    FilterActivity filterActivity = new FilterActivity();
-    String[] age1 = {"Anyone"};
-    String[] age2 = {"Children"};
-    String[] age3 = {"Families"};
-    String[] age4 = {"Young adults"};
-    String[] age5 = {""};
-    String[] age6 = {"Children", "Families"};
-    HomelessShelter shelter1 = new HomelessShelter("Shelter 1", "23", "none",
+    private FilterActivity filterActivity = new FilterActivity();
+    private String[] age1 = {"Anyone"};
+    private String[] age2 = {"Children"};
+    private String[] age3 = {"Families"};
+    private String[] age4 = {"Young adults"};
+    private String[] age5 = {""};
+    private String[] age6 = {"Children", "Families"};
+    private HomelessShelter shelter1 = new HomelessShelter("Shelter 1", "23", "none",
             "123.2331", "231.2312", "213 ensc sdss", "404-232-2342",
             "N/A", "Male", new ArrayList<>(Arrays.asList(age1)),
             "23", 23);
-    HomelessShelter shelter2 = new HomelessShelter("Shelter 2", "23", "none",
+    private HomelessShelter shelter2 = new HomelessShelter("Shelter 2", "23", "none",
             "123.2331", "231.2312", "213 ensc sdss", "404-232-2342",
             "N/A", "Male", new ArrayList<>(Arrays.asList(age2)),
             "23", 23);
-    HomelessShelter shelter3 = new HomelessShelter("Shelter 3", "23", "none",
+    private HomelessShelter shelter3 = new HomelessShelter("Shelter 3", "23", "none",
             "123.2331", "231.2312", "213 ensc sdss", "404-232-2342",
             "N/A", "Male", new ArrayList<String>(Arrays.asList(age3)),
             "23", 23);
-    HomelessShelter shelter4 = new HomelessShelter("Shelter 4", "23", "none",
+    private HomelessShelter shelter4 = new HomelessShelter("Shelter 4", "23", "none",
             "123.2331", "231.2312", "213 ensc sdss", "404-232-2342",
             "N/A", "Male", new ArrayList<String>(Arrays.asList(age4)),
             "23", 23);
-    HomelessShelter shelter5 = new HomelessShelter("Shelter 5", "23", "none",
+    private HomelessShelter shelter5 = new HomelessShelter("Shelter 5", "23", "none",
             "123.2331", "231.2312", "213 ensc sdss", "404-232-2342",
             "N/A", "Male", new ArrayList<String>(Arrays.asList(age5)),
             "23", 23);
-    HomelessShelter shelter6 = new HomelessShelter("Shelter 5", "23", "none",
+    private HomelessShelter shelter6 = new HomelessShelter("Shelter 6", "23", "none",
             "123.2331", "231.2312", "213 ensc sdss", "404-232-2342",
             "N/A", "Male", new ArrayList<String>(Arrays.asList(age6)),
             "23", 23);
-    List<HomelessShelter> shelters = new ArrayList<>();
-    List<HomelessShelter> allShelters = new ArrayList<>();
+    private List<HomelessShelter> shelters = new ArrayList<>();
+    private List<HomelessShelter> allShelters = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+        allShelters.addAll(Arrays.asList(shelter1, shelter2, shelter3, shelter4, shelter5, shelter6));
+    }
 
     @Test
-    public void testIsAgeEmpty() {
-        shelters.addAll(Arrays.asList(shelter1, shelter2, shelter3, shelter4, shelter5, shelter6));
-        filterActivity.filterAge(shelters, allShelters, "Anyone");
+    public void testWhenAgeIsEmpty() {
+        shelters = filterActivity.filterAge(shelters, allShelters, "");
         assertEquals(6, shelters.size());
+        assertEquals("Shelter 1", shelters.get(0).getName());
+        assertEquals("Shelter 2", shelters.get(1).getName());
+        assertEquals("Shelter 3", shelters.get(2).getName());
+        assertEquals("Shelter 4", shelters.get(3).getName());
+        assertEquals("Shelter 5", shelters.get(4).getName());
+        assertEquals("Shelter 6", shelters.get(5).getName());
+    }
+
+    @Test
+    public void testWhenAgeIsAnyone() {
+        shelters = filterActivity.filterAge(shelters, allShelters, "Anyone");
+        assertEquals(1, shelters.size());
+        assertEquals("Shelter 1", shelters.get(0).getName());
+    }
+
+    @Test
+    public void testWhenAgeIsChildren() {
+        shelters = filterActivity.filterAge(shelters, allShelters, "Children");
+        assertEquals(2, shelters.size());
+        assertEquals("Shelter 2", shelters.get(0).getName());
+        assertEquals("Shelter 6", shelters.get(1).getName());
+    }
+
+    @Test
+    public void testWhenAgeIsFamilies() {
+        shelters = filterActivity.filterAge(shelters, allShelters, "Families");
+        assertEquals(2, shelters.size());
+        assertEquals("Shelter 3", shelters.get(0).getName());
+        assertEquals("Shelter 6", shelters.get(1).getName());
+    }
+
+    @Test
+    public void testWhenAgeIsYoungAdults() {
+        shelters = filterActivity.filterAge(shelters, allShelters, "Young adults");
+        assertEquals(1, shelters.size());
+        assertEquals("Shelter 4", shelters.get(0).getName());
     }
 }
